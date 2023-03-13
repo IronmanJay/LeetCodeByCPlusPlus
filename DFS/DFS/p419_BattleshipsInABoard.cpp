03a64b6054e9c4daf3a3c7a8a736618107c213e8
IronmanJay <1975686676@qq.com>
2023-03-13 09:32:59 +0800

    2023-03-13

---
+++ DFS/DFS/p419_BattleshipsInABoard.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	int countBattleships(vector<vector<char>>& board)
+	{
+		int m = board.size();
+		int n = board[0].size();
+		int count = 0;
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if ((board[i][j] == 'X') && (i == 0 || board[i - 1][j] == '.') && (j == 0 || board[i][j - 1] == '.'))
+				{
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+};
