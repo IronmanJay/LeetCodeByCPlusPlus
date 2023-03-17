a2ea7865f3df309e7e1b0d003f7b3ee9fee0b750
IronmanJay <1975686676@qq.com>
2023-03-17 09:55:44 +0800

    2023-03-17

---
+++ DoublePointer/DoublePointer/p1089_DuplicateZeros.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void duplicateZeros(int* arr, int arrSize)
+{
+	int i = -1;
+	int top = 0;
+	while (top < arrSize)
+	{
+		i++;
+		if (arr[i] != 0)
+		{
+			top++;
+		}
+		else
+		{
+			top += 2;
+		}
+	}
+	int j = arrSize - 1;
+	if (top == arrSize + 1)
+	{
+		arr[j] = 0;
+		i--;
+		j--;
+	}
+	while (j >= 0)
+	{
+		arr[j] = arr[i];
+		j--;
+		if (arr[i] == 0)
+		{
+			arr[j] = arr[i];
+			j--;
+		}
+		i--;
+	}
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
