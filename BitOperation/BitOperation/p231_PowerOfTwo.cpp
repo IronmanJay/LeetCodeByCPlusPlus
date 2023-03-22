ed09e46bfa51de81e1c67c5e138b1f9c948567d5
IronmanJay <1975686676@qq.com>
2023-03-22 10:44:37 +0800

    2023-03-22

---
+++ BitOperation/BitOperation/p231_PowerOfTwo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool isPowerOfTwo(int n) {
+		return n > 0 && (n & (n - 1)) == 0;
+	}
+};
+
+/*Ö÷º¯ÊıÊ¡ÂÔ*/
+int main()
+{
+	return 0;
+}
